54a946cb13f88333ee64750d9dd24d6f1257af92
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-05-15 07:27:09 +0300

    Configured Supabase auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Auth } from "@supabase/auth-ui-react"
+import { ThemeSupa } from "@supabase/auth-ui-shared"
+import supabase from "./config/supabaseClient"
 
 // pages
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
 
-
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <Auth 
+        supabaseClient={supabase} 
+        appearance={{ theme: ThemeSupa }}
+        theme="light"
+        providers={['google','github']} // optional
+      />
+    )
+  }
+
   return (
     <BrowserRouter>
       <nav>
         <h1>My Todos</h1>
-        <Link to="/">Home</Link>
-        <Link to="/create">Create New Todo</Link>
+        <div className="order-by">
+          <Link to="/">Home</Link>
+          <Link to="/create">Create New Todo</Link>
+          <button onClick={() => supabase.auth.signOut()} className="form button">Logout</button>
+        </div>
+
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -20,7 +52,7 @@ function App() {
         <Route path="/:id" element={<Update />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
